c42cc4707aecc1d8c08ae65b964d0ae863de3c88
VAIBHAV GUPTA <146114384+114-Vaibhav@users.noreply.github.com>
2025-07-23 22:50:24 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,21 @@
 
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+
 // https://vitejs.dev/config/
 // export default defineConfig({
 //   plugins: [react()],
 // })
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
+  resolve: {
     proxy: {
       '/api': 'https://spssvmicsmartschoolbackend-production.up.railway.app/',
+    },
+    alias: {
+      axios: path.resolve(__dirname, 'src/api.js'), // 👈 Key line
     },
   },
 });
