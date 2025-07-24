8bdac9638b8a2811224b84655d7c6fef0b4d3bc1
VAIBHAV GUPTA <146114384+114-Vaibhav@users.noreply.github.com>
2025-07-24 11:35:36 +0530

    Update Register.jsx

--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -196,7 +196,7 @@ const Register = () => {
             <p className="text-sm text-gray-600">
               Already have an account?{" "}
               <Link
-                to="/admission-registration"
+                to="/admission-login"
                 className="font-medium text-indigo-600 hover:text-indigo-500"
               >
                 Sign in
